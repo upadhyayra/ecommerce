6c326b61ee8fc278e0032889988ca0ff53a898de
upadhyayra <rahulupadhyay1324@gmail.com>
2023-11-23 15:38:06 +0530

    r

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios';
 import { Contextapi } from './Contextapi';
 import ProductCard from './ProductCard';
+import Loading from './assets/loading.gif'
 
 
 function Home() {
@@ -18,18 +19,33 @@ function Home() {
     setUser([...user,item])
   }
 
-  return (
+  return data == "" ? (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "500px",
+      }}
+    >
+      <img src={Loading} alt="" style={{ width: "10%" , height:"20%"}} />
+    </div>
+  ) : (
     <div>
       <div className="container mb-5">
-        <h2 className='mt-5'>Products</h2>
+        <h2 className="mt-5">Products</h2>
         <div className="row card-container">
-          {data.map((item, index) =>
-            <ProductCard item={item} index={index}  hadleAddCart={hadleAddCart} />
-          )}
+          {data.map((item, index) => (
+            <ProductCard
+              item={item}
+              index={index}
+              hadleAddCart={hadleAddCart}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
